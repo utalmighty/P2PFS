2e37f8c267ec1ca16adfa1a762026cd83e775b53
Utkarsh <utalmighty@gmail.com>
2023-05-03 21:07:45 +0530

    Principal Tests

--- src/main/java/com/Huduk/P2PFS/Config/UserHandshakeHandler.java
+++ src/main/java/com/Huduk/P2PFS/Config/UserHandshakeHandler.java
@@ -4,16 +4,22 @@ import java.security.Principal;
 import java.util.Map;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
-public class UserHandshakeHandler extends DefaultHandshakeHandler{
+public class UserHandshakeHandler extends DefaultHandshakeHandler {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserHandshakeHandler.class);
+
 
 	@Override
 	protected Principal determineUser(ServerHttpRequest request,
 			WebSocketHandler wsHandler, Map<String, Object> attributes) {
 		String randomId = UUID.randomUUID().toString();
+		logger.error(randomId);
 		return new PrincipalImpl(randomId);
 	}
 	
